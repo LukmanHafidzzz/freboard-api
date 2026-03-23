f0ea099449770d3700e425c03827f88ac992efdf
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2026-03-23 21:37:40 +0700

    refactor: update database migrations

--- internal/models/product.go
+++ internal/models/product.go
@@ -6,13 +6,11 @@ type Product struct {
 	ID        int       `json:"id"`
 	BrandID   int       `json:"brand_id"`
 	ShapeID   int       `json:"shape_id"`
-	SeriesID  int       `json:"series_id"`
 	ModelName string    `json:"model_name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 
 	Brand         *Brand         `json:"brand,omitempty"`
 	BodyShape     *BodyShape     `json:"body_shape,omitempty"`
-	Series        *Series        `json:"series,omitempty"`
 	Specification *Specification `json:"specification,omitempty"`
 }
